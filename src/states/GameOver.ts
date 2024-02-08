5081e4aff056e7e2efbb7a71b7bfa477a1dfce96
Lucas Oliveira <lucas.henrique@minu.co>
2024-02-08 14:49:53 -0300

    feat: styling game over state

--- src/states/GameOver.ts
+++ src/states/GameOver.ts
@@ -1,5 +1,8 @@
 import * as Phaser from 'phaser';
 
+interface Data {
+  score: number;
+}
 export default class GameOverState extends Phaser.Scene {
   private score: number;
   private highScore: number;
@@ -8,8 +11,62 @@ export default class GameOverState extends Phaser.Scene {
     super('game-over');
   }
 
-  init(data: { score: number }) {
+  init(data: Data) {
     this.score = data.score;
+    this.setCache();
+  }
+
+  create() {
+    let width = Number(this.sys.game.config.width);
+    let height = Number(this.sys.game.config.height);
+
+    const gameOverTitle = this.add.text(width / 2, height / 4, 'Fim de Jogo!', {
+      fontSize: '46px',
+      color: '#fff',
+      align: 'center',
+    }).setOrigin(0.5);
+
+    const scoreText = this.add.text(width / 2, height / 2 - 50, `Pontuação: ${this.score}`, {
+      fontSize: '24px',
+      color: '#fff',
+      align: 'center',
+    }).setOrigin(0.5);
+
+    const highscoreText = this.add.text(width / 2, height / 2, `Recorde: ${this.highScore}`, {
+      fontSize: '24px',
+      color: '#fff',
+      align: 'center',
+    }).setOrigin(0.5);
+
+    const newGameButton = this.add.text(width / 2, height / 2 + 50, 'Novo Jogo', {
+      fontSize: '24px',
+      color: '#000000',
+      backgroundColor: '#ddd',
+      padding: {
+        left: 10,
+        right: 10,
+        top: 5,
+        bottom: 5,
+      },
+    }).setOrigin(0.5).setInteractive();
+
+    newGameButton.on('pointerover', () => {
+      newGameButton.setStyle({
+        backgroundColor: '#888',
+      });
+    });
+    newGameButton.on('pointerout', () => {
+      newGameButton.setStyle({
+        backgroundColor: '#ddd',
+      });
+    });
+    
+    newGameButton.on('pointerdown', () => {
+      this.scene.start('main');
+    });
+  }
+
+  private setCache() {
     const highScoreCache: number = this.cache.text.get('highScore');
     
     if(!highScoreCache || this.score > highScoreCache) {
@@ -18,21 +75,5 @@ export default class GameOverState extends Phaser.Scene {
     } else {
       this.highScore = highScoreCache;
     }
-  }
-
-  create() {
-    this.add.text(16, 16, `Pontos: ${this.score}`, { fontSize: '32px', color: '#fff' });
-    this.add.text(16, 64, `Recorde: ${this.highScore}`, { fontSize: '32px', color: '#fff' });
-
-    const newGameButton = this.add.text(16, 120, 'Novo jogo', { fontSize: '32px', color: '#fff' });
-    // const exitButton = this.add.text(16, 160, 'Exit', { fontSize: '32px', color: '#fff' });
-
-    newGameButton.setInteractive().on('pointerdown', () => {
-      this.scene.start('main');
-    });
-
-    // exitButton.setInteractive().on('pointerdown', () => {
-    //   this.scene.start('boot');
-    // });
   }
 }
